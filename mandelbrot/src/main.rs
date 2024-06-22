25135176d94d0496b77b370681d077bf0835f22e
Tamicktom <rickpokemon38@gmail.com>
2024-06-22 17:39:43 -0300

    ok

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,10 +1,11 @@
 use num::Complex;
+use std::str::FromStr;
 
 fn main() {
-    let c = Complex {re: 2., im: 0.};
+    let c = Complex { re: 2., im: 0. };
     match escape_time(c, 100) {
         Some(count) => println!("Escaped after {} iterations", count),
-        None => println!("Didn't escape")
+        None => println!("Didn't escape"),
     }
 }
 
@@ -31,8 +32,8 @@ fn main() {
 //     }
 // }
 
-fn escape_time(c:Complex<f64>, limit: usize) -> Option<usize> {
-    let mut z = Complex {re: 0., im: 0.};
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex { re: 0., im: 0. };
 
     for i in 0..limit {
         if z.norm_sqr() > 4.0 {
@@ -42,4 +43,23 @@ fn escape_time(c:Complex<f64>, limit: usize) -> Option<usize> {
     }
 
     return None;
+}
+
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+            (Ok(l), Ok(r)) => Some((l, r)),
+            _ => None,
+        },
+    }
+}
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("", ','), None);
+    assert_eq!(parse_pair::<i32>("10,", ','), None);
+    assert_eq!(parse_pair::<i32>(",10", ','), None);
+    assert_eq!(parse_pair::<i32>("10,20", ','), Some((10, 20)));
+    assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
 }
